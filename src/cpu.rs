4fa0f1fd495e9584155850e4ff9631345723bfbf
Tommi Pakarinen <tommi.pakarinen@sanoma.com>
2025-06-07 23:03:50 +0300

    add TAX instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,6 +2,7 @@ pub struct CPU {
   pub pc: u8,
   pub status: u8,
   pub reg_a: u8,
+  pub reg_x: u8,
 }
 
 impl CPU {
@@ -10,6 +11,7 @@ impl CPU {
           pc: 0,
           status: 0,
           reg_a: 0,
+          reg_x: 0,
       }
   }
   
@@ -36,6 +38,21 @@ impl CPU {
                       self.status = self.status & 0b0111_1111;
                   }
               }
+              0xAA => {
+                self.reg_x = self.reg_a;
+
+                if self.reg_x == 0 {
+                  self.status = self.status | 0b0000_0010;
+                } else {
+                    self.status = self.status & 0b1111_1101;
+                }
+
+                if self.reg_x & 0b1000_0000 != 0 {
+                    self.status = self.status | 0b1000_0000;
+                } else {
+                    self.status = self.status & 0b0111_1111;
+                }
+              }
               0x00 => {
                   return;
               }
@@ -56,18 +73,50 @@ mod test {
  use super::*;
 
  #[test]
- fn test_0xa9_lda_immediate_load_data() {
-     let mut cpu = CPU::new();
-     cpu.interpret(vec![0xa9, 0x05, 0x00]);
-     assert_eq!(cpu.reg_a, 0x05);
-     assert!(cpu.status & 0b0000_0010 == 0b00);
-     assert!(cpu.status & 0b1000_0000 == 0);
- }
+  fn test_0xa9_lda_immediate_load_data() {
+    let mut cpu = CPU::new();
+    cpu.interpret(vec![0xa9, 0x05, 0x00]);
+    assert_eq!(cpu.reg_a, 0x05);
+    assert!(cpu.status & 0b0000_0010 == 0b00);
+    assert!(cpu.status & 0b1000_0000 == 0);
+  }
 
   #[test]
   fn test_0xa9_lda_zero_flag() {
-      let mut cpu = CPU::new();
-      cpu.interpret(vec![0xa9, 0x00, 0x00]);
-      assert!(cpu.status & 0b0000_0010 == 0b10);
+    let mut cpu = CPU::new();
+    cpu.interpret(vec![0xa9, 0x00, 0x00]);
+    assert!(cpu.status & 0b0000_0010 == 0b10);
   }
+
+  #[test]
+  fn test_0xa9_lda_neg_flag() {
+    let mut cpu = CPU::new();
+    cpu.interpret(vec![0xa9, 0xFF, 0x00]);
+    assert!(cpu.status & 0b1000_0000 != 0);
+  }
+
+  #[test]
+   fn test_0xaa_tax_immediate_load_data() {
+     let mut cpu = CPU::new();
+     cpu.reg_a = 5;
+     cpu.interpret(vec![0xaa, 0x00]);
+     assert_eq!(cpu.reg_x, 0x05);
+     assert!(cpu.status & 0b0000_0010 == 0b00);
+     assert!(cpu.status & 0b1000_0000 == 0);
+   }
+ 
+   #[test]
+   fn test_0xaa_tax_zero_flag() {
+     let mut cpu = CPU::new();
+     cpu.reg_a = 0;
+     cpu.interpret(vec![0xaa, 0x00]);
+     assert!(cpu.status & 0b0000_0010 == 0b10);
+   }
+   #[test]
+   fn test_0xaa_tax_neg_flag() {
+     let mut cpu = CPU::new();
+     cpu.reg_a = 255;
+     cpu.interpret(vec![0xaa, 0x00]);
+     assert!(cpu.status & 0b1000_0000 != 0);
+   }
 }
