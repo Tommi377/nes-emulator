93af5fc66c4d8ba2665615bc41cd4b2d38851512
Tommi Pakarinen <tommi.pakarinen@sanoma.com>
2025-07-01 14:36:05 +0300

    chr-rom bin

---
+++ src/bin/chr-rom.rs
@@ -0,0 +1,123 @@
+use std::{env, fs};
+
+use nes_emulator::{cpu::CPU, mem::rom::Rom};
+use sdl2::{event::Event, keyboard::Keycode, pixels::PixelFormatEnum, sys::KeyCode};
+
+const DEFAULT_FILE_PATH: &str = "nestest.nes";
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Snake game", (32.0 * 10.0) as u32, (32.0 * 10.0) as u32)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas.set_scale(10.0, 10.0).unwrap();
+
+    let creator = canvas.texture_creator();
+    let mut texture = creator
+        .create_texture_target(PixelFormatEnum::RGB24, 32, 32)
+        .unwrap();
+
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = args.get(1).map(|s| s.as_str()).unwrap_or(DEFAULT_FILE_PATH);
+    let raw = fs::read(file_path).expect("Should have been able to read the file");
+
+    let rom = Rom::new(&raw).unwrap();
+
+    let tile_frame = show_tile(&rom.chr_rom, 1, 0);
+    texture.update(None, &tile_frame.data, 256 * 3).unwrap();
+    canvas.copy(&texture, None, None).unwrap();
+    canvas.present();
+
+    loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => std::process::exit(0),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn show_tile(chr_rom: &[u8], bank: usize, tile_n: usize) -> Frame {
+    let mut frame = Frame::new();
+
+    let tile = &chr_rom[(bank * 0x1000 + tile_n * 16)..(bank * 0x1000 + tile_n * 16 + 16)];
+    for y in 0..8 {
+        let mut plane_lo = tile[y];
+        let mut plane_hi = tile[y + 8];
+
+        for x in 0..8 {
+            let value = (plane_lo & 1) | (plane_hi & 1) << 1;
+            plane_lo <<= 1;
+            plane_hi <<= 1;
+            let rgb = match value {
+                0 => SYSTEM_PALLETE[0x01],
+                1 => SYSTEM_PALLETE[0x23],
+                2 => SYSTEM_PALLETE[0x27],
+                3 => SYSTEM_PALLETE[0x30],
+                _ => panic!("can't be"),
+            };
+            frame.set_pixel(x, y, rgb);
+        }
+    }
+    frame
+}
+
+pub struct Frame {
+    pub data: Vec<u8>,
+}
+
+impl Frame {
+    const WIDTH: usize = 256;
+    const HIGHT: usize = 240;
+
+    pub fn new() -> Self {
+        Frame {
+            data: vec![0; (Frame::WIDTH) * (Frame::HIGHT) * 3],
+        }
+    }
+
+    pub fn set_pixel(&mut self, x: usize, y: usize, rgb: (u8, u8, u8)) {
+        let base = y * 3 * Frame::WIDTH + x * 3;
+        if base + 2 < self.data.len() {
+            self.data[base] = rgb.0;
+            self.data[base + 1] = rgb.1;
+            self.data[base + 2] = rgb.2;
+        }
+    }
+}
+
+impl Default for Frame {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[rustfmt::skip]
+// 2C02 (NTSC) PPU Color Palette
+pub static SYSTEM_PALLETE: [(u8,u8,u8); 64] = [
+   (0x80, 0x80, 0x80), (0x00, 0x3D, 0xA6), (0x00, 0x12, 0xB0), (0x44, 0x00, 0x96), (0xA1, 0x00, 0x5E),
+   (0xC7, 0x00, 0x28), (0xBA, 0x06, 0x00), (0x8C, 0x17, 0x00), (0x5C, 0x2F, 0x00), (0x10, 0x45, 0x00),
+   (0x05, 0x4A, 0x00), (0x00, 0x47, 0x2E), (0x00, 0x41, 0x66), (0x00, 0x00, 0x00), (0x05, 0x05, 0x05),
+   (0x05, 0x05, 0x05), (0xC7, 0xC7, 0xC7), (0x00, 0x77, 0xFF), (0x21, 0x55, 0xFF), (0x82, 0x37, 0xFA),
+   (0xEB, 0x2F, 0xB5), (0xFF, 0x29, 0x50), (0xFF, 0x22, 0x00), (0xD6, 0x32, 0x00), (0xC4, 0x62, 0x00),
+   (0x35, 0x80, 0x00), (0x05, 0x8F, 0x00), (0x00, 0x8A, 0x55), (0x00, 0x99, 0xCC), (0x21, 0x21, 0x21),
+   (0x09, 0x09, 0x09), (0x09, 0x09, 0x09), (0xFF, 0xFF, 0xFF), (0x0F, 0xD7, 0xFF), (0x69, 0xA2, 0xFF),
+   (0xD4, 0x80, 0xFF), (0xFF, 0x45, 0xF3), (0xFF, 0x61, 0x8B), (0xFF, 0x88, 0x33), (0xFF, 0x9C, 0x12),
+   (0xFA, 0xBC, 0x20), (0x9F, 0xE3, 0x0E), (0x2B, 0xF0, 0x35), (0x0C, 0xF0, 0xA4), (0x05, 0xFB, 0xFF),
+   (0x5E, 0x5E, 0x5E), (0x0D, 0x0D, 0x0D), (0x0D, 0x0D, 0x0D), (0xFF, 0xFF, 0xFF), (0xA6, 0xFC, 0xFF),
+   (0xB3, 0xEC, 0xFF), (0xDA, 0xAB, 0xEB), (0xFF, 0xA8, 0xF9), (0xFF, 0xAB, 0xB3), (0xFF, 0xD2, 0xB0),
+   (0xFF, 0xEF, 0xA6), (0xFF, 0xF7, 0x9C), (0xD7, 0xE8, 0x95), (0xA6, 0xED, 0xAF), (0xA2, 0xF2, 0xDA),
+   (0x99, 0xFF, 0xFC), (0xDD, 0xDD, 0xDD), (0x11, 0x11, 0x11), (0x11, 0x11, 0x11)
+];
--- src/mem/rom.rs
+++ src/mem/rom.rs
@@ -9,7 +9,7 @@ const NES_TAG: [u8; 4] = [0x4E, 0x45, 0x53, 0x1A];
 const PRG_ROM_PAGE_SIZE: usize = 16384;
 const CHR_ROM_PAGE_SIZE: usize = 8192;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Rom {
     pub prg_rom: Vec<u8>,
     pub chr_rom: Vec<u8>,
